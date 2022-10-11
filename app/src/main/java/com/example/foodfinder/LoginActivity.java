14390ada4670e4ab39bb71c039ed658f5808f3ba
Shayan Zahedanaraki <shayan.zahedana@gmail.com>
2022-10-10 18:01:35 -0700

    Finished both the Login and the signup pages design, connections and replacements.

--- app/src/main/java/com/example/foodfinder/LoginActivity.java
+++ app/src/main/java/com/example/foodfinder/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.foodfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button toMain = findViewById(R.id.LoginBtn);
+        toMain.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("loggedIn", true);
+            startActivity(intent);
+        });
+
+        Button toSignup = findViewById(R.id.SignupBtn);
+        toSignup.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SignupActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/foodfinder/MainActivity.java
+++ app/src/main/java/com/example/foodfinder/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.foodfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean loggedIn = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            loggedIn = getIntent().getExtras().getBoolean("loggedIn");
+        } catch (Exception ignored) {
+        }
+
+        if (!loggedIn) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+
     }
+
 }

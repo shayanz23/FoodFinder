61da5611f6f16d0c788809d99e7d80812adb1f76
Shayan Zahedanaraki <shayan.zahedana@gmail.com>
2022-10-11 19:10:40 -0700

    Finished both the Login and the signup pages design, connections and replacements.

--- app/src/main/java/com/example/foodfinder/SignupActivity.java
+++ app/src/main/java/com/example/foodfinder/SignupActivity.java
@@ -2,7 +2,9 @@ package com.example.foodfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button toMain = findViewById(R.id.CreateAccountBtn);
+        toMain.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("loggedIn", true);
+            startActivity(intent);
+        });
     }
 }

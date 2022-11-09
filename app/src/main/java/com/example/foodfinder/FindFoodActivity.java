3513ec5e222baca672c8139ec54a1ed815af7be2
Shayan Zahedaraki <shayan.zahedana@gmail.com>
2022-11-09 14:25:43 -0800

    made the google maps api work.

--- app/src/main/java/com/example/foodfinder/FindFoodActivity.java
+++ app/src/main/java/com/example/foodfinder/FindFoodActivity.java
@@ -1,37 +1,73 @@
 package com.example.foodfinder;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import android.os.Bundle;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class FindFoodActivity extends AppCompatActivity implements OnMapReadyCallback {
     SupportMapFragment mapFragment;
+    private FusedLocationProviderClient fusedLocationClient;
+    private double mLatitude;
+    private double mLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_findfood);
-
-        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapAPI);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+                mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapAPI);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
     }
 
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         LatLng sydney = new LatLng(-34, 151);
-        googleMap.addMarker(new MarkerOptions().position(sydney).title("Sydney"));
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        fusedLocationClient.getLastLocation()
+//                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+//                    @Override
+//                    public void onSuccess(Location location) {
+//                        // Got last known location. In some rare situations this can be null.
+//                        if (location != null) {
+//                            // Logic to handle location object
+//                            mLatitude = location.getLatitude();
+//                            mLongitude = location.getLongitude();
+//                        }
+//                    }
+//                });
+//        LatLng currentLocation = new LatLng(mLatitude,mLatitude);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("myLatLng"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
     }
 

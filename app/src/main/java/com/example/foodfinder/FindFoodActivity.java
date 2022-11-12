3ac5b5ddf30e57d51a722bf88428154ffca73800
Shayan Zahedanaraki <shayan.zahedana@gmail.com>
2022-11-11 19:03:32 -0800

    got everything working for the JSON response except getting a proper JSON response, because I can't get my API key to work on the google maps api URL.

--- app/src/main/java/com/example/foodfinder/FindFoodActivity.java
+++ app/src/main/java/com/example/foodfinder/FindFoodActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,7 +13,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -23,20 +31,32 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class FindFoodActivity extends AppCompatActivity implements OnMapReadyCallback {
     SupportMapFragment mapFragment;
     private FusedLocationProviderClient fusedLocationClient;
     private double mLatitude;
     private double mLongitude;
+    private Integer count;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_findfood);
+
+        Bundle bundle = getIntent().getExtras();
+        String apiKey = getString(R.string.googlemapsAPI);
+        String codeToSearch = bundle.getString("codeToSearch");
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
                 mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapAPI);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
+        AsyncTaskRunner runner = new AsyncTaskRunner();
+        runner.execute(apiKey);
 
     }
 
@@ -44,29 +64,6 @@ public class FindFoodActivity extends AppCompatActivity implements OnMapReadyCal
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         LatLng sydney = new LatLng(-34, 151);
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            // TODO: Consider calling
-//            //    ActivityCompat#requestPermissions
-//            // here to request the missing permissions, and then overriding
-//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//            //                                          int[] grantResults)
-//            // to handle the case where the user grants the permission. See the documentation
-//            // for ActivityCompat#requestPermissions for more details.
-//            return;
-//        }
-//        fusedLocationClient.getLastLocation()
-//                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-//                    @Override
-//                    public void onSuccess(Location location) {
-//                        // Got last known location. In some rare situations this can be null.
-//                        if (location != null) {
-//                            // Logic to handle location object
-//                            mLatitude = location.getLatitude();
-//                            mLongitude = location.getLongitude();
-//                        }
-//                    }
-//                });
-//        LatLng currentLocation = new LatLng(mLatitude,mLatitude);
         googleMap.addMarker(new MarkerOptions().position(sydney).title("myLatLng"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
     }
@@ -75,6 +72,36 @@ public class FindFoodActivity extends AppCompatActivity implements OnMapReadyCal
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("loggedIn", true);
         startActivity(intent);
+    }
+
+
+    private class AsyncTaskRunner extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            RequestQueue queue = Volley.newRequestQueue(FindFoodActivity.this);
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, strings[0], null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+
+                    try {
+                        JSONArray resultArray = response.getJSONArray("response");
+                        for(int i = 0; i < 5; i++) {
+                            //FOR THE FIRST 5 ENTRIES IN THE RESULT ARRAY
+                            JSONObject obj = resultArray.getJSONObject(i);
+                            System.out.println(obj);
+                        }
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, error -> Toast.makeText(FindFoodActivity.this, error.toString(), Toast.LENGTH_LONG).show());
+            queue.add(request);
+            return null;
+        }
     }
 
 }
